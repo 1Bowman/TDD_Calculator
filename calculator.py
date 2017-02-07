564d4a1bc5661c289e6880d85392da40783cb50c
Bowman <onlyonebowman@gmail.com>
2017-02-06 19:14:24 -0600

    Added functionality and test for the multiplication function

--- calculator.py
+++ calculator.py
@@ -5,8 +5,13 @@ def calc():
 def add(val1, val2):
     return float(val1) + float(val2)
 
+
 def sub(val1, val2):
     return float(val1) - float(val2)
+
+
+def mul(val1, val2):
+    return float(val1) * float(val2)
 
 
 def take_user_input():
