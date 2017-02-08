1904d9185e6faef7e61a06dc77b99bbdb095d4a8
Bowman <onlyonebowman@gmail.com>
2017-02-07 19:37:29 -0600

    Added a view with regular expression

--- calculator.py
+++ calculator.py
@@ -1,5 +1,14 @@
-def calc():
-    return "Hello World"
+import re
+
+
+def view():
+    while True:
+        input_string = take_user_input()
+        pattern = re.search('\d*(\.\d+)?', input_string)
+        if pattern:
+            print(pattern)
+        else:
+            print("Incorrect expression")
 
 
 def add(val1, val2):
@@ -19,7 +28,7 @@ def div(val1, val2):
 
 
 def take_user_input():
-    return float(input())
+    return str(input())
 
 if __name__ == "__main__":
-    calc()
+    view()
